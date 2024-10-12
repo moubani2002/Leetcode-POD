c386db78813a22fc79db6e2f825c4f6c7ebdac67
MOUBANI SHEE <165894661+moubani2002@users.noreply.github.com>
2024-10-12 10:07:17 +0530

    day2

---
+++ divide_interval_into_minimal_groups.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int minGroups(vector<vector<int>>& intervals) {
+        vector<int>fr(SHRT_MAX * SCHAR_MAX);             // 1
+        for(auto i: intervals) fr[i[0]]++, fr[i[1]+1]--; // 2
+        partial_sum(begin(fr), end(fr), begin(fr));      // 3
+        return *max_element(begin(fr), end(fr));
+    }
+};
